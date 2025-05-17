7ecd8f976e0bc94c529cdfef94c80ae79a9859bd
Marc Lawrence Magadan <135830304+1mRen@users.noreply.github.com>
2025-05-18 00:13:03 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,9 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 // Allow CORS requests from specified origins
-const allowedOrigins = process.env.CORS_ORIGINS ? process.env.CORS_ORIGINS.split(',') : ['http://localhost:4200'];
+const allowedOrigins = process.env.CORS_ORIGINS
+  ? process.env.CORS_ORIGINS.split(',')
+  : ['http://localhost:4200', 'https://user-management-system-ibgs.onrender.com'];
 app.use(cors({
   origin: (origin, callback) => {
     if (!origin || allowedOrigins.includes(origin)) {
