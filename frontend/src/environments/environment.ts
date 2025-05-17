8123c3dec3840cb5a85d6cfd13cd5c25c2f1e895
Marc Lawrence Magadan <135830304+1mRen@users.noreply.github.com>
2025-05-18 00:26:40 +0800

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
 	production: false,
-	apiUrl: "http://localhost:4000",
-	//apiUrl: "https://user-management-system-ibgs.onrender.com",
+	//apiUrl: "http://localhost:4000",
+	apiUrl: "https://user-management-system-ibgs.onrender.com"
 };
 
 /*
